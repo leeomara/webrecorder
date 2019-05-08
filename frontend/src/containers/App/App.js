b3153bb99adab4b5b57912ebf59e0ef9b0959af2
Mark Beasley <mbeasl1@gmail.com>
2019-05-07 20:57:03 -0400

    Minor UI tweaks for desktop release addresses #5

--- frontend/src/containers/App/App.js
+++ frontend/src/containers/App/App.js
@@ -274,7 +274,7 @@ export class App extends Component {
             </section>
         }
         {
-          hasFooter &&
+          hasFooter && !__DESKTOP__ &&
             <Footer />
         }
       </React.Fragment>
